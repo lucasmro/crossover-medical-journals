6efca919e67ad25676e6ae9ae77d1b6ef6943b81
lucasmro <lucasmro@gmail.com>
2016-05-22 18:02:24 -0300

    Add CrossOriginFilter configuration

--- src/main/java/com/crossover/medical/journals/MedicalJournalsApplication.java
+++ src/main/java/com/crossover/medical/journals/MedicalJournalsApplication.java
@@ -1,5 +1,11 @@
 package com.crossover.medical.journals;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration.Dynamic;
+
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,9 +32,22 @@ public class MedicalJournalsApplication extends Application<Configuration> {
         environment.jersey().register(new ApplicationResource());
 
         environment.healthChecks().register("MedicalJournalsApplication", new MedicalJournalsHealthCheck());
+
+        configureCors(environment);
     }
 
     public static void main(final String[] args) throws Exception {
         new MedicalJournalsApplication().run(args);
+    }
+
+    private void configureCors(Environment environment) {
+        final Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,PUT,POST,DELETE,OPTIONS");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        filter.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+        filter.setInitParameter("allowedHeaders",
+                "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
+        filter.setInitParameter("allowCredentials", "true");
     }
 }
