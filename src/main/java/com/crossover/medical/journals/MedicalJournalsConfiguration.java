92a7201e50d538858bb3b811ae7d933814651587
lucasmro <lucasmro@gmail.com>
2016-05-22 18:35:02 -0300

    Add database configuration

--- src/main/java/com/crossover/medical/journals/MedicalJournalsConfiguration.java
+++ src/main/java/com/crossover/medical/journals/MedicalJournalsConfiguration.java
@@ -1,12 +1,32 @@
 package com.crossover.medical.journals;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
 
 public class MedicalJournalsConfiguration extends Configuration {
 
     private String applicationName;
 
+    @Valid
+    @NotNull
+    private DataSourceFactory database = new DataSourceFactory();
+
     public String getApplicationName() {
         return applicationName;
+    }
+
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory() {
+        return database;
+    }
+
+    @JsonProperty("database")
+    public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
+        database = dataSourceFactory;
     }
 }
